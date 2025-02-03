47412b6d4f58059ca5d44234d946ddc48aee6f24
YoungRa19 <randyna19@gmail.com>
2025-02-02 23:11:40 -0600

    Getting main working

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sportsapp/sportsapp/app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +16,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      )
+      ),
+        home: Sports(),
     );
   }
 }
